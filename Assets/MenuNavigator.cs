e2b775cfde37983e3ed62ae0b8ff973f4019f78d
Cheftian <septianto238b@gmail.com>
2025-07-27 14:15:38 +0700

    first commit

---
+++ Assets/MenuNavigator.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuNavigator : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Game"); 
+    }
+
+    public void OpenSettings()
+    {
+        SceneManager.LoadScene("Settings");
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void OpenCredits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game...");
+        Application.Quit();
+    }
+}
